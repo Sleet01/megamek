bdf87ecda2887fdc0efe284dadc6f8607b01df5d
sleet01 <sleet01@hotmail.com>
2026-01-12 01:12:51 -0800

    Start on adding IContact definition

--- megamek/src/megamek/common/units/ObscuredEntity.java
+++ megamek/src/megamek/common/units/ObscuredEntity.java
@@ -1,4 +1,32 @@
 package megamek.common.units;
 
 public class ObscuredEntity implements IContact {
+
+    private Entity entity;
+    private int forcesLevel;
+    private int positionLevel;
+    private int logisticsLevel;
+    private int personnelLevel;
+
+    public ObscuredEntity(
+          Entity entity, int forcesLevel, int positionLevel, int logisticsLevel, int personnelLevel
+    ) {
+        this.entity = entity;
+        this.forcesLevel = forcesLevel;
+        this.positionLevel = positionLevel;
+        this.logisticsLevel = logisticsLevel;
+        this.personnelLevel = personnelLevel;
+    }
+
+    public Entity getEntity() {
+        return entity;
+    }
+
+    public Crew getCrew() {
+        return entity.getCrew();
+    }
+
+    public String getShortName() {
+        return entity.getShortName();
+    }
 }
